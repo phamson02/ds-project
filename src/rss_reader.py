722713133a12ccaf7da29acfb33804a54d0c9e27
phamson02 <phamtienson02@gmail.com>
2022-12-01 13:23:44 +0700

    exclude some links

--- src/rss_reader.py
+++ src/rss_reader.py
@@ -1,15 +1,26 @@
 import feedparser
 
 # Scrape all articles' link from rss_link
-def scrape_rss(rss_link):
+def scrape_rss(rss_link, category=None):
     rss = feedparser.parse(rss_link)
+
+    # load all excluded sources
+    with open('docs/excluded-sources.txt', 'r') as f:
+        excluded_sources = f.read().splitlines()
+
+    print(excluded_sources)
+
     articles = []
     for item in rss['items']:
-        article = [item['link'], item['title'], item['published']]
-        articles.append(article)
+        # check if source is excluded
+        excluded = any(source in item['link'] for source in excluded_sources)
+        if not excluded:
+            article = [item['link'], item['title'], item['published'], category]
+            articles.append(article)
     return articles
 
 if __name__ == '__main__':
     rss_link = 'https://vnexpress.net/rss/thoi-su.rss'
+    category = 'Thời sự'
     articles = scrape_rss(rss_link)
     print(*articles, sep='\n')
