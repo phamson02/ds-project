b583d58d00c095575b3424a3f8523a4232b3f85a
phamson02 <phamtienson02@gmail.com>
2023-02-10 14:43:11 +0700

    update

---
+++ src/compute_score.py
@@ -0,0 +1,37 @@
+import argparse
+import networkx as nx
+import pandas as pd
+import tqdm
+
+
+def main(args):
+    G = nx.Graph()
+
+    df_link = pd.read_csv(args.directory + "link.csv", index_col="id")
+    df_ner = pd.read_csv(args.directory + "ner.csv", index_col="id")
+
+    # Add edges and weights
+    for index, row in df_link.iterrows():
+        G.add_edge(row['from'], row['to'], weight=row['weight'])
+
+    nodes = []
+    eigenvector_cents = []
+    ec_dict = nx.eigenvector_centrality(G, max_iter=1000, weight='weight')
+    for node in tqdm(G.nodes()):
+        nodes.append(node)
+        eigenvector_cents.append(ec_dict[node])
+    centrality_map = {}
+    for n, c in zip(nodes, eigenvector_cents):
+        centrality_map[n] = c
+
+    df_ner['score'] = df_ner['id'].apply(lambda x: centrality_map.get(x, 0.0))
+
+    df_ner.to_csv(args.directory + "ner_score.csv", index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Compute betweenness centrality score for each entity')
+    parser.add_argument('-d', '--directory', help='Path to directory containing link and ner csv files',
+                        default='data/')
+    args = parser.parse_args()
+    main(args)
