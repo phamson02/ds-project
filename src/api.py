af561b5911f91c73c2bc0cba4d112ad71d6b57e5
phamson02 <phamtienson02@gmail.com>
2023-12-21 16:34:34 +0000

    update api

--- src/api.py
+++ src/api.py
@@ -2,26 +2,29 @@ import argparse
 import time
 import pandas as pd
 import requests
+import os
 
 
 def main(args):
-    url = 'https://lionfish-app-pnbyg.ondigitalocean.app/api/'
+    # url = 'https://9bdd-128-199-214-29.ngrok-free.app/api/'
+    # Use environment variable to store url instead of hardcoding it here
+    url = os.environ.get("API_URL")
 
     # if args.input file is empty, return
     if pd.read_csv(args.input).empty:
         return
-    
-    if args.type == 'article':
-        url += 'article'
+
+    if args.type == "article":
+        url += "article"
 
         id_arr = []
         for chunk in pd.read_csv(args.input, chunksize=100):
             articles = chunk
-            articles.drop('id', axis=1, inplace=True)
-            articles.drop('content', axis=1, inplace=True)
-            articles_dict = articles.to_dict('records')
+            articles.drop("id", axis=1, inplace=True)
+            articles.drop("content", axis=1, inplace=True)
+            articles_dict = articles.to_dict("records")
             r = requests.post(url, json=articles_dict)
-            
+
             if r.status_code == 200:
                 id_arr += r.json()
             else:
@@ -35,22 +38,22 @@ def main(args):
                     if r.status_code == 200:
                         id_arr += r.json()
                         break
-            
+
         # update id column
         if id_arr:
             articles = pd.read_csv(args.input)
-            articles['id'] = id_arr
+            articles["id"] = id_arr
             articles.to_csv(args.input, index=False)
 
-    elif args.type == 'node':
-        url += 'node'
+    elif args.type == "node":
+        url += "node"
 
         id_arr = []
         for chunk in pd.read_csv(args.input, chunksize=300):
             nodes = chunk
-            nodes.drop('id', axis=1, inplace=True)
-            nodes.rename(columns={'entity': 'name'}, inplace=True)
-            nodes_dict = nodes.to_dict('records')
+            nodes.drop("id", axis=1, inplace=True)
+            nodes.rename(columns={"entity": "name"}, inplace=True)
+            nodes_dict = nodes.to_dict("records")
 
             r = requests.post(url, json=nodes_dict)
 
@@ -70,41 +73,49 @@ def main(args):
 
         if id_arr:
             nodes = pd.read_csv(args.input)
-            nodes['id'] = id_arr
+            nodes["id"] = id_arr
             nodes.to_csv(args.input, index=False)
 
-    elif args.type == 'edge':
-        url += 'edge'
+    elif args.type == "edge":
+        url += "edge"
 
         for chunk in pd.read_csv(args.input, chunksize=20):
             edges = chunk
-            edges.drop('id', axis=1, inplace=True)
-            edges.rename(columns={'from': 'source', 'to': 'target', 'article_ids': 'articles', 'weight': 'size'}, inplace=True)
-            edges_dict = edges.to_dict('records')
+            edges.drop("id", axis=1, inplace=True)
+            edges.rename(
+                columns={
+                    "from": "source",
+                    "to": "target",
+                    "article_ids": "articles",
+                    "weight": "size",
+                },
+                inplace=True,
+            )
+            edges_dict = edges.to_dict("records")
             for edge in edges_dict:
-                edge['articles'] = eval(edge['articles'])
-                edge['size'] = int(edge['size'])
+                edge["articles"] = eval(edge["articles"])
+                edge["size"] = int(edge["size"])
 
             r = requests.post(url, json=edges_dict)
 
             print(r.status_code)
             if r.status_code != 200:
-
                 cnt_tries = 0
                 while r.status_code != 200 and cnt_tries < 3:
-                    
                     time.sleep(5)
                     r = requests.post(url, json=edges_dict)
                     if r.status_code == 200:
                         break
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Upload data to Node.js server')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Upload data to Node.js server")
 
-    parser.add_argument('--input', type=str, required=True, help='Input file')
+    parser.add_argument("--input", type=str, required=True, help="Input file")
 
-    parser.add_argument('--type', type=str, required=True, help='Type of data to insert')
+    parser.add_argument(
+        "--type", type=str, required=True, help="Type of data to insert"
+    )
 
     args = parser.parse_args()
     main(args)
